d326548eb3a494c626e2cda00fa0e63701a2f0d9
Lauriano <laurianocarlos@hotmail.com>
2024-01-26 10:24:08 -0300

    Adicionado página de contato

--- widget/Contato.php
+++ widget/Contato.php
@@ -1,1 +1,53 @@
-CONTATO
+<div class="main_contact py-5 bg-ligth text-center">
+    <div class="container">
+        <h1 class="text-front">Entre em Contato Conosco</h1>
+        <p class="mb-0">Quer conversar com um corretor exclusivo e ter o atendimento diferenciado em busca do seu imóvel dos sonhos?</p>
+        <p>Preencha o formulário abaixo e vamos lhe direcionar para alguém que entende a sua necessidade!</p>
+
+        <div class="row text-start">
+            <form action="">
+                <h2 class="icon-envelope text-black-50">Envie um e-mail</h2>
+                <div class="form-group">
+                    <input type="text" class="form-control" placeholder="Insira seu nome">
+                    <input type="email" class="form-control" placeholder="Insira seu e-mail">
+                    <input type="tel" class="form-control" placeholder="Insira seu número">
+                    <textarea name="texto" rows="5" class="form-control mb-3" placeholder="Digite sua mensagem..."></textarea>
+                </div>
+
+                <div class="form-group text-end">
+                    <button class="btn btn-front">Enviar Contao</button>
+                </div>
+
+            </form>
+        </div>
+    </div>
+</div>
+
+<div class="main_contact_types py-5">
+    <div class="container">
+        <div class="row">
+            <div class="col-12 col-md-6 col-lg-4 mb-3 text-center">
+                <h2 class="icon-envelope">Por E-mail</h2>
+                <p>Nossos atendentes irão entrar em contato com você assim que possivel.</p>
+
+                <p class="pt-3"><a href="#" class="text-front text-decoration-none">contato@laraveldev.com.br</a></p>
+            </div>
+            <div class="col-12 col-md-6 col-lg-4 mb-3 text-center">
+                <h2 class="icon-phone">Por Telefone</h2>
+                <p>Estamos Disponível no número abaixo no horário comercial.</p>
+
+                <p class="pt-3"><a href="#" class="text-front text-decoration-none">(11) 23455-7891</a></p>
+            </div>
+            <div class="col-12 col-md-6 col-lg-4 mb-3 text-center">
+                <h2 class="icon-share-alt">Por Redes Sociais</h2>
+                <p>Fique por dentro de tudo que a gente compartilha em nossas redes sociais.</p>
+
+                <p>
+                    <button class="btn btn-front icon-facebook icon-notext"></button>
+                    <button class="btn btn-front icon-instagram icon-notext"></button>
+                    <button class="btn btn-front icon-twitter icon-notext"></button>
+                </p>
+            </div>
+        </div>
+    </div>
+</div>
