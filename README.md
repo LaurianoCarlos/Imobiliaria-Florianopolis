a510f64bd5dd67b828f69c4db30a83d0e5b1a7e7
Lauriano <laurianocarlos@hotmail.com>
2024-01-27 01:45:23 -0300

    Adicionado página de propriedades e Readme

--- README.md
+++ README.md
@@ -1,6 +1,22 @@
-<<<<<<< HEAD
-# Imobiliária-Florianópolis
-=======
-# Imobiliria-Florianpolis
->>>>>>> 67223cf0d02c1a76377e6cd7fb12b3d17d0f0223
-# Imobiliaria-Florianopolis
+# Página de Imobiliária com Bootstrap 5 e SASS
+
+Este projeto é uma página de imobiliária desenvolvida com Bootstrap 5 e SASS.
+
+## Estrutura de Arquivos
+
+- **index.html**: Página principal.
+- **src/**
+    - **variables.scss**: Variáveis SASS.
+    - **fold/**: Estilos personalizados.
+    - **pages/**: Estilos personalizados.
+    - **Theme/**: Estilos personalizados Cabeçalho e Rodapé.
+- **widget/**: Páginas do Projeto.
+- **assets/**: Armazena imagens do projeto.
+- **package.json**: Arquivo de configuração/Dependências.
+
+## Instruções de Instalação
+
+Certifique-se de ter o Node.js instalado. Em seguida, execute o seguinte comando para instalar as dependências:
+
+```bash
+npm install
---
+++ widget/propriedade.php
@@ -0,0 +1,136 @@
+<div class="main_property">
+    <div class="main_property_header bg-ligth py-5">
+        <div class="container">
+            <h1 class="text-front">Linda Casa no Rio Tavares com Vista para o Mar</h1>
+            <p class="mb-0">Imóvel Residencial - Apartamento - Campeche</p>
+        </div>
+    </div>
+
+    <div class="main_property_content py-md-5">
+        <div class="container">
+            <div class="row">
+                <div class="col-12 col-md-8 px-3 px-md-0">
+                    <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+                        <div class="carousel-inner">
+                            <div class="carousel-item active">
+                                <img src="assets/images/properties/1/5a3571ab-4d76-466f-8246-eff8cb98cedd.jpg" class="d-block w-100" alt="...">
+                            </div>
+                            <div class="carousel-item">
+                                <img src="assets/images/properties/1/8f317473-e7b0-4c6e-8198-2ebe44a21f11.jpg" class="d-block w-100" alt="...">
+                            </div>
+                            <div class="carousel-item">
+                                <img src="assets/images/properties/1/6abf463b-67a0-4fee-847e-42537699ad8e.jpg" class="d-block w-100" alt="...">
+                            </div>
+                        </div>
+                        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Próximo</span>
+                        </button>
+                        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Anterior</span>
+                        </button>
+                    </div>
+
+                    <div class="main_property_content_price text-muted py-2">
+                        <p class="main_property_content_price_iptu">IPTU: R$ 100,00</p>
+                        <p>valor do Aluguel: R$ 2.400,00/Mês</p>
+                    </div>
+                    <div class="main_property_content_description pt-4">
+                        <h2 class="text-front">Conheça mais o Imóvel</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam consequuntur cum earum
+                            eum in incidunt inventore libero maiores nisi officiis quam, quia quisqua.</p>
+                    </div>
+                    <div class="main_property_content_features">
+                        <h2>Caracteristicas</h2>
+                        <table class="table table-striped">
+                            <tbody>
+                                <tr>
+                                    <td>Dormitórios</td>
+                                    <td>1</td>
+                                </tr>
+                                <tr>
+                                    <td>Suítes</td>
+                                    <td>1</td>
+                                </tr>
+                                <tr>
+                                    <td>Banheiros</td>
+                                    <td>2</td>
+                                </tr>
+                                <tr>
+                                    <td>Salas</td>
+                                    <td>2</td>
+                                </tr>
+                                <tr>
+                                    <td>Garagem</td>
+                                    <td>2</td>
+                                </tr>
+                                <tr>
+                                    <td>Garagem Coberta</td>
+                                    <td>2</td>
+                                </tr>
+                                <tr>
+                                    <td>Área Total</td>
+                                    <td>250 m°</td>
+                                </tr>
+                                <tr>
+                                    <td>Área Util</td>
+                                    <td>180 m°</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+
+                    <div class="main_property_content_structure">
+                        <h2 class="text-front"> Estrutura</h2>
+                        <span class="btn btn-success icon-check my-1 my-lg-0"> Ar-Condicionado</span>
+                        <span class="btn btn-success icon-check my-1 my-lg-0"> Churrasqueria</span>
+                        <span class="btn btn-success icon-check my-1 my-lg-0"> Despensa</span>
+                        <span class="btn btn-success icon-check my-1 my-lg-0"> Escritório</span>
+                    </div>
+                </div>
+
+                <div class="col-12 col-md-4 py-5 py-md-0">
+                    <a href="#" class="btn-custom-whatsapp icon-whatsapp mb-4">
+                        <span class="ms-1"></span> Converse com um Corretor</a>
+
+                    <div class="main_property_contract">
+                        <h2 class="bg-front text-white">Entre em Contato</h2>
+
+                        <form action="">
+                            <div class="form-group">
+                                <label for="">Seu Nome:</label>
+                                <input type="text" class="form-control" placeholder="Informe seu nome completo">
+                            </div>
+                            <div class="form-group">
+                                <label for="">Seu Telefone:</label>
+                                <input type="tel" class="form-control" placeholder="Informe seu telefone">
+                            </div>
+                            <div class="form-group">
+                                <label for="">Seu Nome:</label>
+                                <input type="email" class="form-control" placeholder="Informe seu e-mail">
+                            </div>
+                            <div class="form-group">
+                                <label for="">Seu Nome:</label>
+                                <textarea name="text" class="form-control" placeholder="Digite sua mensagem"></textarea>
+                            </div>
+
+                            <div class="form-group">
+                                <div class="d-grid">
+                                    <button type="submit" class="btn btn-front  mt-3">Enviar</button>
+                                </div>
+                                <p class="text-center text-muted mt-3">(11) 23455-7891</p>
+                            </div>
+                        </form>
+                    </div>
+
+                    <div class="main_property_share text-center mt-3">
+                        <a href="#" class="btn btn-front icon-facebook icon-notext"></a>
+                        <a href="#" class="btn btn-front icon-twitter icon-notext"></a>
+                        <a href="#" class="btn btn-front icon-instagram icon-notext"></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
